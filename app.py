cd0cc2709d0fa742d5dfbd20fd683ff38b5d1d54
Andrew-Sams <152635248+Andrew-Sams@users.noreply.github.com>
2024-01-11 20:18:53 -0600

    Update app.py with a very basic version for testing

--- app.py
+++ app.py
@@ -1,11 +1,5 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="Natalie's App",
-    layout="wide",
-)
-
-st.title("Welcome to Natalie's App")
-
-if st.button('Press Me'):
-    st.text("Natalie")
+st.title("Simple App")
+if st.button('Click Me'):
+    st.write("Natalie")
